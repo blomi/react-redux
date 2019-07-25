43f143f1b8b2e07dceffd3aedd236fac530b5dc0
blomi <blomi11@freeuni.edu.ge>
2019-07-25 21:35:13 +0400

    bla

---
+++ src/components/courses/ManageCoursePage.js
@@ -0,0 +1,73 @@
+import React, { useEffect, useState } from "react";
+import { connect } from "react-redux";
+import * as courseActions from "../../redux/actions/courseActions";
+import * as authorActions from "../../redux/actions/authorActions";
+import PropTypes from "prop-types";
+import CourseForm from "./CourseForm";
+import { newCourse } from "../../../tools/mockData";
+
+function ManageCoursePage({
+  courses,
+  authors,
+  loadAuthors,
+  loadCourses,
+  ...props
+}) {
+  const [course, setCourse] = useState({ ...props.course });
+  const [errors, setErrors] = useState({});
+  useEffect(() => {
+    if (courses.length === 0) {
+      loadCourses().catch(error => {
+        alert("loading courses failed " + error);
+      });
+    }
+    if (authors.length === 0) {
+      loadAuthors().catch(error => {
+        alert("loading authors failed " + error);
+      });
+    }
+  }, []);
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setCourse(prevCourse => ({
+      ...prevCourse,
+      [name]: name === "authorId" ? parseInt(value, 10) : value
+    }));
+  }
+
+  return (
+    <CourseForm
+      course={course}
+      errors={errors}
+      authors={authors}
+      onChange={handleChange}
+    />
+  );
+}
+
+ManageCoursePage.propTypes = {
+  course: PropTypes.object.isRequired,
+  courses: PropTypes.array.isRequired,
+  authors: PropTypes.array.isRequired,
+  loadCourses: PropTypes.func.isRequired,
+  loadAuthors: PropTypes.func.isRequired
+};
+
+function mapStateToProps(state) {
+  return {
+    course: newCourse,
+    courses: state.courses,
+    authors: state.authors
+  };
+}
+
+const mapDispatchToProps = {
+  loadCourses: courseActions.loadCourses,
+  loadAuthors: authorActions.loadAuthors
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(ManageCoursePage);
